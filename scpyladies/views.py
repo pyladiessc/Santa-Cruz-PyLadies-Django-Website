3b83dfa5b725b8592957f1bc6fcb828680e86b8c
Kathleen Tuite (laptop) <kathleen.tuite@gmail.com>
2018-10-01 19:30:53 -0700

    Resolved merge conflict

--- scpyladies/views.py
+++ scpyladies/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world!!!! Editing from github")
+    return HttpResponse("Hello, world!!!! Conflict Resolution!")
